7e8107c0f656e42c77a34ab38843217f4e20eaa9
Akash Mehra <mehra.akash@gmail.com>
2015-03-17 17:52:20 -0700

    Fix bugs
    range takes a range of values (doesn't implicitly take the starting value as 1 if the starting value is missing)
    removed the overloaded function, it acted like a duplicate.
    if shuffle is nil then we assume that shuffle is required.

--- cross-validate.lua
+++ cross-validate.lua
@@ -12,10 +12,13 @@ cross_validate = {}
 
 function cross_validate.splitDataSet(input,splitSize,shuffle)
     if input ~= nil and input:nDimension() >= 1 then
+        if shuffle == nil then
+            shuffle = true
+        end
         if shuffle == true then
             indices = torch.randperm(input:size(1)):long()
         else
-            indices = torch.range(input:size(1)):long()
+            indices = torch.range(1,input:size(1)):long()
         end
         
         intPart,fracPart = math.modf(splitSize * input:size(1))
@@ -29,17 +32,3 @@ function cross_validate.splitDataSet(input,splitSize,shuffle)
     end
     return nil
 end
-
--- splits the dataset into train and validation sets based on the splitSize (a float) passed in as an argument.
--- the function signature is [validationSet,trainSet] splitDataSet (input, splitSize,shuffle)
--- input: Input Tensor (should have atleast one dimension
--- splitSize: a floating point value ranging between [0,1], for instance 0.3 (technically inclusive
--- but doesn't make sense to call this function with the interval endpoints).
--- It by default shuffles the dataset.
--- Note: The input Tensor remains the same even if shuffling is turned on.
-
-function cross_validate.splitDataSet(input, splitSize)
-  return cross_validate.splitDataSet(input, splitSize, true)
-end
-
-return cross_validate
